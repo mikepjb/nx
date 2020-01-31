9fb289b493c8b0c6d043b00a6c861525e714a4a2
Michael Bruce <me@michaelbruce.co>
2020-01-31 19:53:33 +0000

    include prestart setup & offline support

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 
 // TODO make nx init.js build arg console.log/build
+// TODO ensure build step uses production flag.
 
 const chalk = require('chalk')
 const fs = require('fs')
@@ -16,31 +17,33 @@ if (args.length != 0) {
 
 console.log(chalk.blue('==>') + ' nx: creating new site, ' + projectName)
 
-var projectDirectory = process.cwd() + '/' + projectName
+var dir = process.cwd() + '/' + projectName
 
-if (!fs.existsSync(projectDirectory)) {
-  fs.mkdirSync(projectDirectory)
+if (!fs.existsSync(dir)) {
+  fs.mkdirSync(dir)
 } else {
-  console.error(chalk.red('! ') + 'the directory, ' + projectDirectory + ' already exists')
+  console.error(chalk.red('! ') + 'the directory, ' + dir + ' already exists')
   process.exit(0)
 }
 
-if (!fs.existsSync(projectDirectory + '/src')) {
-  fs.mkdirSync(projectDirectory + '/src')
-}
+if (!fs.existsSync(dir + '/src')) fs.mkdirSync(dir + '/src')
 
 // Templates {{{
 
 var packageJson =
 `{
-  "name": "js-sandbox",
+  "name": "${projectName}",
   "version": "1.0.0",
   "main": "index.js",
   "license": "MIT",
   "scripts": {
-    "start": "[ ! -d node_modules ] && npm i; babel-node init.js",
     "build": "babel-node init.js build",
-    "dev": "npm start"
+    "build:css": "postcss ./src/style.css -o ./public/style.css",
+    "dev": "npm start",
+    "ensure:css": "[ ! -f ./public/style.css ] && npm run build:css",
+    "ensure:lib": "[ ! -d node_modules ] && npm install --prefer-offline",
+    "prestart": "npm run ensure:lib; npm run ensure:css",
+    "start": "[ ! -d node_modules ] && npm i; babel-node init.js"
   },
   "dependencies": {
     "@babel/core": "^7.8.3",
@@ -69,34 +72,52 @@ var packageJson =
 }`
 
 var init =
-`
-import express from 'express'
+`import express from 'express'
 import fs from 'fs'
 import WebSocket from 'ws'
 import chokidar from 'chokidar'
+import process from 'process'
 
-var server = express()
-var reloadingSocket = new WebSocket.Server({port: 7777})
-var fileWatcher = chokidar.watch('./src')
+var args = process.argv.splice(2)
 
-reloadingSocket.on('connection', () => console.log('browser connected'))
-fileWatcher.on('ready', () => {
-  fileWatcher.on('all', () => {
-    console.log('file changed')
-    Object.keys(require.cache).forEach((id) => {
-      if (/[\\\/\\\\]src[\\\/\\\\]/.test(id)) delete require.cache[id]
-      reloadingSocket.clients.forEach((client) => {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send('reload!')
-        }
+var devServer = () => {
+  var server = express()
+  var reloadingSocket = new WebSocket.Server({port: 7777})
+  var fileWatcher = chokidar.watch('./src')
+
+  reloadingSocket.on('connection', () => console.log('browser connected'))
+  fileWatcher.on('ready', () => {
+    fileWatcher.on('all', () => {
+      console.log('file changed')
+      Object.keys(require.cache).forEach((id) => {
+        if (/[\\\/\\\\]src[\\\/\\\\]/.test(id)) delete require.cache[id]
+        reloadingSocket.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send('reload!')
+          }
+        })
       })
     })
   })
-})
 
-// server.get('/', (req, res, next) => require('./src/index')(req, res, next))
-server.get('/', (req, res, next) => res.json({"status": "started"}))
-server.listen(8080, '0.0.0.0', () => console.log('server started on 8080'))
+  server.get('/', (req, res, next) => require('./src/index')(req, res, next))
+  server.get('/style.css', (req, res) =>
+    res.type('css') && res.sendFile(__dirname + '/public/style.css'))
+  server.get('/status.json', (req, res, next) => res.json({"status": "started"}))
+  server.listen(8080, '0.0.0.0', () => console.log('server started on 8080'))
+}
+
+var build = () => {
+  console.log('building content.. writing to ./public')
+  if (!fs.existsSync('./public')) fs.mkdirSync('./public')
+  fs.writeFileSync('public/index.html', 'something.')
+}
+
+if (args.length != 0 && args[0] === 'build') {
+  build()
+} else {
+  devServer()
+}
 `
 
 var postcssConfig =
@@ -140,10 +161,10 @@ import ReactDOMServer from 'react-dom/server'
 import Express from 'express'
 import fs from 'fs'
 
-const routes = {
-  '/': 'page',
-  '/lore': 'lore',
-}
+const Sun = () =>
+ <svg height="24" width="24" class="fill-current text-red-300">
+  <circle cx="20" cy="20" r="10" strokeWidth="8"/>
+</svg> 
 
 const Page = (content) =>
   \`<!DOCTYPE html>
@@ -163,7 +184,7 @@ class Index extends React.Component {
   render() {
     return <div class="bg-gray-100 text-gray-900 antialiased min-h-screen">
       <nav class="p-5 bg-gray-900 text-gray-100 flex justify-between text-lg shadow-md">
-        <div>Dawn of a new site</div>
+        <div><Sun/>Dawn of a new site</div>
         <div>some menu items</div>
       </nav>
       <div class="m-5">Welcome.</div>
@@ -172,11 +193,7 @@ class Index extends React.Component {
 }
 
 var staticContent = ReactDOMServer.renderToStaticMarkup(<Index/>)
-
-if (!fs.existsSync('./public')) {
-  fs.mkdirSync('./public')
-}
-fs.writeFileSync('public/index.html', Page(staticContent))
+module.exports = (req, res) => res.send(Page(staticContent)) 
 `
 
 // }}}
@@ -194,14 +211,14 @@ function write(name, dir, content) {
   }
 }
 
-console.log(projectDirectory)
+console.log(dir)
 
-write('init.js', projectDirectory, init)
-write('package.json', projectDirectory, packageJson)
-write('postcss.config.js', projectDirectory, postcssConfig)
-write('tailwind.config.js', projectDirectory, tailwindConfig)
-write('src/style.css', projectDirectory, style)
-write('src/index.js', projectDirectory, index)
+write('init.js', dir, init)
+write('package.json', dir, packageJson)
+write('postcss.config.js', dir, postcssConfig)
+write('tailwind.config.js', dir, tailwindConfig)
+write('src/style.css', dir, style)
+write('src/index.js', dir, index)
 
 // TODO src/index.js
 // TODO initial yarn dev/build commands
